13856dc6db47b4e9f88fead13b5defdf884651d1
Faith Ege <faithege@hotmail.co.uk>
2021-10-23 10:41:53 +0100

    Add readline game code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-const myConst = false;
+// @ts-ignore
+import readline from 'readline-promise'; 
+import { evaluateMachinePlay, generateSlots } from './game';
+import { Machine, Player, State } from './model';
 
-export { myConst };
+
+
+async function repl(readline:any){ //game play here
+    const initialMachineFund = 10
+    const initialScreen = generateSlots()
+    const initialMachine: Machine = {
+        screen: initialScreen,
+        fundAmount: initialMachineFund,
+        playCost: 1
+    }
+    const initialPlayer: Player = {
+        walletAmount: 6
+    }
+
+    // one mutable variable, no need to affect initial constants
+    let state: State = {
+        machine: initialMachine,
+        player: initialPlayer
+    }
+    // could log history of state with state[], and current state would be first/last element
+    
+    do {
+        console.log(state.machine.screen)
+        console.log(`Your wallet contains: ${state.player.walletAmount}}`)
+
+
+        await readline.questionAsync("Press enter to pull the lever ->")
+        const nextScreen = generateSlots()
+        
+        state = evaluateMachinePlay(state, nextScreen)
+    }
+    while (true)
+    
+}
+
+// Start Game - top level async function then immediately called using ()
+(async () => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: true
+        })
+
+    try {
+        await repl(rl)
+    } catch (error) {
+        console.log(error)
+    }
+    finally {
+        rl.close()
+    }
+})(); // IIFE - calls the function immediately
